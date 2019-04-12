8da49676403c37691cd51b4e9bafcf3f6c42a58b
Louis <louis@lacompany.com>
2019-04-12 14:08:01 +0200

    redirects not found to the default to address

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ var forwardTo = process.env.TO_ADDRESS;
 exports.handler = function(event, context) {
     
     // Airtable init
-    var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base(AIRTABLE_BASE);
-    const table = base(process.env.AIRTABLE_TABLE);
+    var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base(process.env.AIRTABLE_BASE);
+    var table = base(process.env.AIRTABLE_TABLE);
 
     // Extract the first member of the email address
     var msgInfo = JSON.parse(event.Records[0].Sns.Message);
     var originalEmail = msgInfo.mail.commonHeaders.to[0];
+
     // var originalEmail = 'louis.rouffineau@jungleprogram.org';
     username = originalEmail.substring(0, originalEmail.indexOf("@"));
 
@@ -28,70 +29,65 @@ exports.handler = function(event, context) {
             return;
         }
         
+        // If user is found, redirect the message to its address
         if (records.length > 0) {
-            // Get the email address to forward to
             forwardTo = records[0].fields.email;
+        }
 
-            // don't process spam messages
-            if (msgInfo.receipt.spamVerdict.status === 'FAIL' || msgInfo.receipt.virusVerdict.status === 'FAIL') {
-                console.log('Message is spam or contains virus, ignoring.');
-                context.succeed();
-            }
+        // don't process spam messages
+        if (msgInfo.receipt.spamVerdict.status === 'FAIL' || msgInfo.receipt.virusVerdict.status === 'FAIL') {
+            console.log('Message is spam or contains virus, ignoring.');
+            context.succeed();
+        }
 
-            var email = msgInfo.content, headers = "From: 'Jungle Program' <"+forwardFrom+">\r\n";
-            headers += "Reply-To: "+msgInfo.mail.commonHeaders.from[0]+"\r\n";
-            headers += "X-Original-To: "+msgInfo.mail.commonHeaders.to[0]+"\r\n";
-            headers += "To: "+forwardTo+"\r\n";
-            headers += "Subject: "+msgInfo.mail.commonHeaders.subject+"\r\n";
+        var email = msgInfo.content, headers = "From: 'Jungle Program' <"+forwardFrom+">\r\n";
+        headers += "Reply-To: "+msgInfo.mail.commonHeaders.from[0]+"\r\n";
+        headers += "X-Original-To: "+msgInfo.mail.commonHeaders.to[0]+"\r\n";
+        headers += "To: "+forwardTo+"\r\n";
+        headers += "Subject: "+msgInfo.mail.commonHeaders.subject+"\r\n";
 
-            if (email) {
-                var res;
-                res = email.match(/Content-Type:.+\s*boundary.*/);
-                if (res) {
-                    headers += res[0]+"\r\n";
-                }
-                else {
-                    res = email.match(/^Content-Type:(.*)/m);
-                    if (res) {
-                        headers += res[0]+"\r\n";
-                    }
-                }
-
-                res = email.match(/^Content-Transfer-Encoding:(.*)/m);
-                if (res) {
-                    headers += res[0]+"\r\n";
-                }
-
-                res = email.match(/^MIME-Version:(.*)/m);
-                if (res) {
-                    headers += res[0]+"\r\n";
-                }
-
-                var splitEmail = email.split("\r\n\r\n");
-                splitEmail.shift();
-
-                email = headers+"\r\n"+splitEmail.join("\r\n\r\n");
+        if (email) {
+            var res;
+            res = email.match(/Content-Type:.+\s*boundary.*/);
+            if (res) {
+                headers += res[0]+"\r\n";
             }
             else {
-                email = headers+"\r\n"+"Empty email";
+                res = email.match(/^Content-Type:(.*)/m);
+                if (res) {
+                    headers += res[0]+"\r\n";
+                }
             }
 
-            new AWS.SES().sendRawEmail({
-                RawMessage: { Data: email }
-            }, function(err, data) {
-                if (err) context.fail(err);
-                else {
-                    console.log('Sent with MessageId: ' + data.MessageId);
-                    context.succeed();
-                }
-            });
+            res = email.match(/^Content-Transfer-Encoding:(.*)/m);
+            if (res) {
+                headers += res[0]+"\r\n";
+            }
 
-        } else {
-            console.log('User not found. Rejecting message.');
-            callback(null, {'disposition':'STOP_RULE_SET'});
-            context.fail();
-            return;
+            res = email.match(/^MIME-Version:(.*)/m);
+            if (res) {
+                headers += res[0]+"\r\n";
+            }
+
+            var splitEmail = email.split("\r\n\r\n");
+            splitEmail.shift();
+
+            email = headers+"\r\n"+splitEmail.join("\r\n\r\n");
         }
+        else {
+            email = headers+"\r\n"+"Empty email";
+        }
+
+        new AWS.SES().sendRawEmail({
+            RawMessage: { Data: email }
+        }, function(err, data) {
+            if (err) context.fail(err);
+            else {
+                console.log('Sent with MessageId: ' + data.MessageId);
+                context.succeed();
+            }
+        });
+
     });
 
 }
