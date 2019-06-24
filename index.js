2f1bb5ae4930b67c803d0f03e55e54d43b6ce41b
Louis <louis@lacompany.com>
2019-06-24 16:06:50 +0200

    Add To, Cc and Bcc support for email forwarding

--- index.js
+++ index.js
@@ -2,56 +2,79 @@ const AWS = require('aws-sdk');
 const Airtable = require('airtable');
 require('dotenv').config()
 
-var forwardFrom = process.env.FROM_ADDRESS;
-var forwardTo = process.env.TO_ADDRESS;
+const forward_from = process.env.FROM_ADDRESS;
+const domain = process.env.DOMAIN;
 
-exports.test = function () {
-  console.log('test');
-};
+const waitFor = (ms) => new Promise(r => setTimeout(r, ms));
+
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+exports.test = async function() {
+  exports.handler(null, null, true)
+}
 
 exports.handler = function(event, context) {
   
   // Airtable init
-  var base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base(process.env.AIRTABLE_BASE);
-  var table = base(process.env.AIRTABLE_TABLE);
+  const base = new Airtable({apiKey: process.env.AIRTABLE_KEY}).base(process.env.AIRTABLE_BASE);
+  const table = base(process.env.AIRTABLE_TABLE);
 
-  // Extract the first member of the email address
-  var msgInfo = JSON.parse(event.Records[0].Sns.Message);
-  var originalEmail = msgInfo.mail.commonHeaders.to[0];
-  username = originalEmail.substring(0, originalEmail.indexOf("@"));
+  // Choose between test or production data
+  if (event === null) {
+    var message_info = require('./test_data.json')
+  } else {
+    var message_info = JSON.parse(event.Records[0].Sns.Message);
+  }
 
-  // Loop through To: and CC recipients
+  // Prepare an array with unique recipients
+  var recipients = [message_info.receipt.recipients[0]];
 
-  // Look for the member with its username
-  table.select({
-    maxRecords: 1,
-    view: 'Grid view',
-    filterByFormula: `AND({username} = "${username}", {active})`
-  }).firstPage((err, records) => {
-    if (err) {
-      console.error(err);
+  // Use our own asyncForEach
+  asyncForEach(recipients, async (recipient) => {
+    [recipient_username, recipient_domain] = recipient.split('@');
+
+    // Filter by domain and lookup redirect email on Airtable
+    if (recipient_domain != domain) {
       return;
     }
-    
-    // If user is found, redirect the message to its address
-    if (records.length > 0) {
-      forwardTo = records[0].fields.email;
-    }
 
-    // don't process spam messages
-    if (msgInfo.receipt.spamVerdict.status === 'FAIL' || msgInfo.receipt.virusVerdict.status === 'FAIL') {
+    // Airtable lookup
+    table.select({
+      maxRecords: 1,
+      view: 'Grid view',
+      filterByFormula: `AND({username} = "${recipient_username}", {active})`
+    }).firstPage((err, records) => {
+      if (records.length > 0) {
+        exports.sendEmail(event, context, message_info, records[0].fields.email);
+        console.log('-- Sending email to: ' + records[0].fields.email);
+      }
+    });
+  })
+
+}
+
+exports.sendEmail = function(event, context, message_info, forward_to) {
+  if (event === null) {
+    console.log('-- TEST MODE -- Not sending an email to:' + forward_to);
+  } else {
+    // Don't process spam messages
+    if (message_info.receipt.spamVerdict.status === 'FAIL' || message_info.receipt.virusVerdict.status === 'FAIL') {
       console.log('Message is spam or contains virus, ignoring.');
       context.succeed();
     }
 
     // Extract sender name
-    var senderName = msgInfo.mail.commonHeaders.from[0].substring(0, msgInfo.mail.commonHeaders.from[0].indexOf(" <"));
+    var senderName = message_info.mail.commonHeaders.from[0].substring(0, message_info.mail.commonHeaders.from[0].indexOf(" <"));
 
-    var email = msgInfo.content, headers = "From: "+senderName+" <"+forwardFrom+">\r\n";
-    headers += "Reply-To: "+msgInfo.mail.commonHeaders.from[0]+"\r\n";
-    headers += "X-Original-To: "+msgInfo.mail.commonHeaders.to[0]+"\r\n";
-    headers += "To: "+forwardTo+"\r\n";
-    headers += "Subject: "+msgInfo.mail.commonHeaders.subject+"\r\n";
+    var email = message_info.content, headers = "From: "+senderName+" <"+forward_from+">\r\n";
+    headers += "Reply-To: "+message_info.mail.commonHeaders.from[0]+"\r\n";
+    headers += "X-Original-To: "+message_info.mail.commonHeaders.to[0]+"\r\n";
+    headers += "To: "+forward_to+"\r\n";
+    headers += "Subject: "+message_info.mail.commonHeaders.subject+"\r\n";
 
     if (email) {
       var res;
@@ -94,7 +117,5 @@ exports.handler = function(event, context) {
         context.succeed();
       }
     });
-
-  });
-
+  }
 }
